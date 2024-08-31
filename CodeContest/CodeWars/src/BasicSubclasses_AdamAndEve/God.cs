944a6dec9d7d1503637df136dbfde915d8dd3814
Naser Rouhi <n.rouhi@asasx.ir>
2024-08-31 18:07:31 +0330

    feat(CodeWars): BasicSubclasses_AdamAndEve contest from CodeWars added.

---
+++ CodeContest/CodeWars/src/BasicSubclasses_AdamAndEve/God.cs
@@ -0,0 +1,18 @@
+﻿namespace BasicSubclasses_AdamAndEve;
+
+public class God
+{
+    public static Human[] Create() => [new Man(), new Woman()];
+}
+
+public class Human
+{
+}
+
+public class Man : Human
+{
+}
+
+public class Woman : Human
+{
+}
