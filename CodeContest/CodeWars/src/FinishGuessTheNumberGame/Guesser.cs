e2816b0eafec634372ace015599ce3cba08b1e93
Naser Rouhi <n.rouhi@asasx.ir>
2024-09-03 12:04:45 +0330

    feat(CodeWars): FinishGuessTheNumberGame contest added.

---
+++ CodeContest/CodeWars/src/FinishGuessTheNumberGame/Guesser.cs
@@ -0,0 +1,26 @@
+﻿namespace FinishGuessTheNumberGame;
+
+public class Guesser
+{
+    private readonly int _correctNumber;
+    private int _remainedLives;
+
+    public Guesser(int correctNumber, int remainedLives)
+    {
+        _correctNumber = correctNumber;
+        _remainedLives = remainedLives;
+    }
+
+    public bool Guess(int guessedNumber)
+    {
+        if (_remainedLives < 1)
+            throw new Exception("You have lost!");
+
+        if (guessedNumber == _correctNumber)
+            return true;
+
+        _remainedLives--;
+
+        return false;
+    }
+}
