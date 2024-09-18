a8a87b4cfe0641e6a0482ddaf51499e9d565aca2
Naser Rouhi <n.rouhi@asasx.ir>
2024-09-18 17:56:18 +0330

    feat(CodeWars): Vectors contest added.

---
+++ CodeContest/CodeWars/src/Vectors/Vector3D.cs
@@ -0,0 +1,10 @@
+﻿namespace Vectors;
+
+public class Vector3D
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+    public double Length => Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+}
